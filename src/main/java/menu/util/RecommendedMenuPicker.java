78208e956e400ec543473322067712361da1de92
mainfn <_@mainfn.dev>
2023-10-04 18:41:04 +0900

    refact: 임의의 메뉴를 뽑을 때, List<Menu>가 아닌 List<String>을 기반으로 변경

--- src/main/java/menu/util/RecommendedMenuPicker.java
+++ src/main/java/menu/util/RecommendedMenuPicker.java
@@ -2,6 +2,7 @@ package menu.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 import menu.domain.category.Category;
 import menu.domain.menu.Menu;
 
@@ -9,6 +10,7 @@ public final class RecommendedMenuPicker {
 
   public Menu pick(final Category category) {
     final List<Menu> menus = Menu.findMenusByCategory(category);
-    return Randoms.shuffle(menus).get(0);
+    final List<String> menuNames = menus.stream().map(Menu::getName).collect(Collectors.toList());
+    return Menu.of(Randoms.shuffle(menuNames).get(0));
   }
 }
