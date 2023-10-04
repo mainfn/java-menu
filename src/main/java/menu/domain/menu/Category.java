e215c72024a06d6e3b2964107f8bcd884a588980
mainfn <_@mainfn.dev>
2023-10-04 12:19:23 +0900

    feat: Category 생성

---
+++ src/main/java/menu/domain/menu/Category.java
@@ -0,0 +1,35 @@
+package menu.domain.menu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Category {
+  JAPENESE("일식", 1),
+  KOREAN("한식", 2),
+  CHINESE("중식", 3),
+  ASIAN("아시안", 4),
+  WESTERN("양식", 5);
+  private static final Map<Integer, Category> mp = new HashMap<>();
+
+  static {
+    for (final Category category : values()) {
+      mp.put(category.code, category);
+    }
+  }
+
+  private final String name;
+  private final int code;
+
+  Category(final String name, final int code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  public static Category of(final int code) {
+    final Category category = mp.get(code);
+    if (category == null) {
+      throw new IllegalArgumentException("유효하지 않은 Category입니다.");
+    }
+    return category;
+  }
+}
