90d58231de8fe7296eace8ec63bc914de161ca1a
mainfn <_@mainfn.dev>
2023-10-04 16:05:40 +0900

    feat: 못 먹는 메뉴 추가 기능

--- src/main/java/menu/domain/menu/UneatableMenus.java
+++ src/main/java/menu/domain/menu/UneatableMenus.java
@@ -1,10 +1,11 @@
 package menu.domain.menu;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class UneatableMenus extends Menus {
-
 
   private UneatableMenus(final List<Menu> menus) {
     super(menus);
@@ -14,4 +15,30 @@ public final class UneatableMenus extends Menus {
     return new UneatableMenus(new ArrayList<>());
   }
 
+  public static UneatableMenus of(final List<Menu> menus) {
+    return new UneatableMenus(menus);
+  }
+
+  public void addAll(final UneatableMenus newMenus) {
+    menus.addAll(newMenus.menus);
+    validateUneatableMenusCount();
+    validateDuplicateMenus();
+  }
+
+  private void validateDuplicateMenus() {
+    final Set<Menu> menusSet = new HashSet<>(menus);
+    if (menusSet.size() != menus.size()) {
+      throw new IllegalArgumentException("중복되는 먹지 못하는 메뉴가 있습니다.");
+    }
+  }
+
+  private void validateUneatableMenusCount() {
+    if (menus.size() > 2) {
+      throw new IllegalArgumentException("먹지 못하는 메뉴는 최대 2개 존재할 수 있습니다.");
+    }
+  }
+
+  public boolean canEat(final Menu menu) {
+    return menus.stream().noneMatch(m -> m == menu);
+  }
 }
