3643d771e0c6bb42e2e9d41adc0fd17ea01b307a
mainfn <_@mainfn.dev>
2023-10-17 20:51:13 +0900

    feat: get Recommended Categories one by one

--- src/main/java/menu/domain/category/RecommendedCategories.java
+++ src/main/java/menu/domain/category/RecommendedCategories.java
@@ -1,10 +1,13 @@
 package menu.domain.category;
 
 import java.util.List;
+import java.util.Optional;
 
 public final class RecommendedCategories {
 
   private final List<Category> categories;
+
+  private int nextIndex = 0;
 
   private RecommendedCategories(final List<Category> categories) {
     this.categories = categories;
@@ -14,7 +17,8 @@ public final class RecommendedCategories {
     return new RecommendedCategories(categories);
   }
 
-  public Category get(final int index) {
-    return categories.get(index);
+  public Optional<Category> get() {
+    return Optional.of(categories.get(nextIndex++));
   }
+
 }
