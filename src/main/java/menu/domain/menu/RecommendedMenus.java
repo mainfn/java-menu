ea61ec4c0507d11941fa6e781dd42d01c7d40d87
mainfn <_@mainfn.dev>
2023-10-17 18:25:32 +0900

    feat: recommending category

---
+++ src/main/java/menu/domain/menu/RecommendedMenus.java
@@ -0,0 +1,13 @@
+package menu.domain.menu;
+
+import java.util.List;
+
+public final class RecommendedMenus {
+
+  private final List<Menu> menus;
+
+  private RecommendedMenus(final List<Menu> menus) {
+    this.menus = menus;
+  }
+
+}
