3d3c514c938b4be199c83c3f079c6b124f15ec0a
mainfn <_@mainfn.dev>
2023-10-17 20:51:57 +0900

    feat: feature that adding recommending to all Coaches

--- src/main/java/menu/domain/coach/Coaches.java
+++ src/main/java/menu/domain/coach/Coaches.java
@@ -2,22 +2,58 @@ package menu.domain.coach;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import menu.domain.category.Category;
+import menu.domain.category.RecommendedCategories;
+import menu.domain.menu.Menu;
+import menu.domain.menu.MenuPicker;
 
 public final class Coaches {
 
   private final List<Coach> coaches;
 
-  private Coaches(final List<Coach> coaches) {
+  private final RecommendedCategories recommendedCategories;
+
+  private Coaches(
+      final List<Coach> coaches,
+      RecommendedCategories recommendedCategories
+  ) {
     this.coaches = coaches;
+    this.recommendedCategories = recommendedCategories;
   }
 
-  public static Coaches from(final List<String> coachNames) {
+  public static Coaches from(
+      final List<String> coachNames,
+      final RecommendedCategories recommendedCategories
+  ) {
     final List<Coach> coaches = new ArrayList<>();
 
     for (final String coachName : coachNames) {
       coaches.add(Coach.of(coachName));
     }
 
-    return new Coaches(coaches);
+    return new Coaches(coaches, recommendedCategories);
+  }
+
+  public void recommend(final MenuPicker menuPicker) {
+    Optional<Category> category = recommendedCategories.get();
+
+    while (category.isPresent()) {
+      Category c = category.get();
+      _recommend(c, menuPicker);
+    }
+  }
+
+  private void _recommend(
+      final Category category,
+      final MenuPicker menuPicker
+  ) {
+    for (final Coach coach : coaches) {
+      Menu menu = menuPicker.pickByCategory(category);
+      while (!coach.canRecommend(menu)) {
+        menu = menuPicker.pickByCategory(category);
+      }
+      coach.addRecommendedMenu(menu);
+    }
   }
 }
